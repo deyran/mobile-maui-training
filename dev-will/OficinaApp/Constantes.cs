3ae9e6e98c964bfb096db6b875d49ff2651de0fc
Deyvid Rannyere <deyran@gmail.com>
2023-09-18 19:38:17 -0300

    ## Curso Maui .Net - Navegando para página de Cadastro - Parte 5 - Crud com Maui + Sqlite
    # Curso Maui .Net
    ## Curso Maui .Net - Navegando para página de Cadastro - Parte 5 - Crud com Maui + Sqlite

--- dev-will/OficinaApp/Constantes.cs
+++ dev-will/OficinaApp/Constantes.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OficinaApp
 {
-    internal class Constantes
+    public static class Constantes
     {
     }
 }
