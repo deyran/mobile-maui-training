6cf5ced6a0d06b98ef8a03b30d0858901c0d2c85
Deyvid Rannyere <deyran@gmail.com>
2023-09-21 14:17:17 -0300

    Maui document test

---
+++ maui-doc/NetDocTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetDocTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ maui-doc/NetDocTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NetDocTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
