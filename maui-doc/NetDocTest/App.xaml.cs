4cabd9d4c3c78e8e22d30586365bdbf5d0ebd3c3
Deyvid Rannyere <deyran@gmail.com>
2023-09-21 14:55:55 -0300

    GridTest

--- maui-doc/NetDocTest/App.xaml.cs
+++ maui-doc/NetDocTest/App.xaml.cs
@@ -8,7 +8,7 @@ namespace NetDocTest
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new VerticalStackLayoutTest());
+            MainPage = new NavigationPage(new GridTest());
         }
     }
 }
