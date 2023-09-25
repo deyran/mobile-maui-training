34a4dca52b28d8ac89e3c46d5b79f9f47abb2c3c
Deyvid Rannyere <deyran@gmail.com>
2023-09-25 15:30:36 -0300

    ApiElnData - GetItemsAsync

--- MauiAPI/APIEln/ApiElnData.cs
+++ MauiAPI/APIEln/ApiElnData.cs
@@ -1,13 +1,15 @@
 ﻿
 
+using Newtonsoft.Json;
+
 namespace MauiAPI.APIEln
 {
     public class ApiElnData
     {
         private readonly HttpClient _client;
-        private readonly string _baseAddress = 
+        private readonly string _baseAddress =
         "https://sgestorweb.eletronorte.com.br/engenharia/segtrabalho/api/";
-        
+
         public ApiElnData()
         {
             _client = new HttpClient();
@@ -21,9 +23,12 @@ namespace MauiAPI.APIEln
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
+                var extintorData = JsonConvert.DeserializeObject<List<ExtintorData>>(content);
+
+                return extintorData;
             }
 
-                return null;
+            return null;
         }
     }
 }
