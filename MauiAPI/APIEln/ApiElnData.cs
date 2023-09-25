876315ffe02fb8d166fdfab1a81c729c06ca9e9b
Deyvid Rannyere <deyran@gmail.com>
2023-09-25 15:16:39 -0300

    ApiElnData

---
+++ MauiAPI/APIEln/ApiElnData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAPI.APIEln
+{
+    public class ApiElnData
+    {
+        private readonly HttpClient _client;
+
+        public ApiElnData()
+        {
+            
+        }
+    }
+}
