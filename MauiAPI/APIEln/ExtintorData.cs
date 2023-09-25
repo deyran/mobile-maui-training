ad7e2bb4f915401bb35741c4d7b031c8a2ec256e
Deyvid Rannyere <deyran@gmail.com>
2023-09-25 15:22:20 -0300

    ExtintorData - Update

---
+++ MauiAPI/APIEln/ExtintorData.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAPI.APIEln
+{
+    public class ExtintorData
+    {
+        public int ExtintorAlmoxarifadoID { set; get; }
+        public string Descricao { set; get; }
+        public string Uf { set; get; }
+        public string ExtintorFisicos { set; get; }
+
+    }
+}
