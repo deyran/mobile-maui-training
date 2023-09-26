43bb78def02c424d60c53fb1288aeed45734f367
Deyvid Rannyere <deyran@gmail.com>
2023-09-26 16:00:22 -0300

    MainViewModel

---
+++ MauiApiRest/ViewsModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MauiApiRest.Models;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+namespace MauiApiRest.ViewsModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://catalogo.macoratti.net/api/1";
+
+        [ObservableProperty]
+        public string _categoriaInfoId;
+        [ObservableProperty]
+        public string _categoriaInfoName;
+        [ObservableProperty]
+        public Categoria _categoria;
+        [ObservableProperty]
+        public ObservableCollection<Categoria> _categorias;
+        [ObservableProperty]
+        private string _nome;
+        [ObservableProperty]
+        private string _imagemUrl;
+    }
+}
