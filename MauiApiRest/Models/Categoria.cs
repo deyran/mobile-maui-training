5270d3bb1edf40b31e007856ee3198438b072631
Deyvid Rannyere <deyran@gmail.com>
2023-09-26 15:11:47 -0300

    Categoria

---
+++ MauiApiRest/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApiRest.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { set; get; }
+        public string Nome { set; get; }
+        public string ImagemUrl { set; get; }
+    }
+}
