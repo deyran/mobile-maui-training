0b5983cda275c40f51c5f4bb0d746549edf0fe5d
Deyvid Rannyere <deyran@gmail.com>
2023-10-03 21:32:30 -0300

    WeatherInfoPage Frame 1

--- MauiWeatherApp/App.xaml.cs
+++ MauiWeatherApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiWeatherApp
+﻿using MauiWeatherApp.Pages;
+
+namespace MauiWeatherApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new WeatherInfoPage();
         }
     }
 }
