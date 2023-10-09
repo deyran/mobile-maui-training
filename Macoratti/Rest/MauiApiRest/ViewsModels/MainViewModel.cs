40a0f75e8fb6663b401e9aaeb623a4c7d7a66ca0
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 15:18:59 -0300

    ## 17:29 - Implementação do padrão MVVM
    ## 17:29 - Implementação do padrão MVVM

---
+++ Macoratti/Rest/MauiApiRest/ViewsModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MauiApiRest.Models;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Input;
+
+namespace MauiApiRest.ViewsModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://catalogo.macoratti.net/api/1";
+
+        [ObservableProperty]
+        public string _categoriaInfoId;
+        [ObservableProperty]
+        public string _categoriaInfoNome;
+        [ObservableProperty]
+        public Categoria _categoria;
+        [ObservableProperty]
+        public ObservableCollection<Categoria> _categorias;
+        [ObservableProperty]
+        private string _nome;
+        [ObservableProperty]
+        private string _imagemUrl;
+
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            Categorias = new ObservableCollection<Categoria>();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+    }
+}
