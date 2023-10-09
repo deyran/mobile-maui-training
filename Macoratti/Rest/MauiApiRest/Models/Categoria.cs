7084db8cf84addb457d1408ed299a4fd5dd5e8af
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 14:06:14 -0300

    ## 10:40 - Definindo modelo de domínio
    # Consumindo serviços REST
    ## 00:13 - Introdução
    ## 10:00 - Construindo app Maui
    ## 10:40 - Definindo modelo de domínio

---
+++ Macoratti/Rest/MauiApiRest/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApiRest.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
