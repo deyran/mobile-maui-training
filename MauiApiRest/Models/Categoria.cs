4d538f1d7a3f757a1f57d080c2664e30142f9896
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 13:18:58 -0300

    Update

--- MauiApiRest/Models/Categoria.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiApiRest.Models
-{
-    public class Categoria
-    {
-        public int CategoriaId { set; get; }
-        public string Nome { set; get; }
-        public string ImagemUrl { set; get; }
-    }
-}
