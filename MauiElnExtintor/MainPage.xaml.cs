42ded270aa6051bf12325ebb83196e06d9f81e78
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 23:12:18 -0300

    Update

--- MauiElnExtintor/MainPage.xaml.cs
+++ MauiElnExtintor/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiElnExtintor
+﻿using MauiElnExtintor.ViewsModels;
+
+namespace MauiElnExtintor
 {
     public partial class MainPage : ContentPage
     {        
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }     
     }
 }
--- MauiElnExtintor/ViewsModels/MainViewModel.cs
+++ MauiElnExtintor/ViewsModels/MainViewModel.cs
@@ -7,7 +7,54 @@ using System.Windows.Input;
 
 namespace MauiElnExtintor.ViewsModels
 {
-    public class MainViewModel
+    public partial class MainViewModel : ObservableObject
     {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://sgestorweb.eletronorte.com.br/engenharia/segtrabalho/api";
+
+        [ObservableProperty]
+        public string _extintorAlmInfoId;
+        [ObservableProperty]
+        public string _extintorAlmInfoDesc;
+        [ObservableProperty]
+        public ExtintorAlmoxarifado _extintorAlmoxarifado;
+        [ObservableProperty]
+        public ObservableCollection<ExtintorAlmoxarifado> _extintorAlmoxarifados;
+        [ObservableProperty]
+        private string _descricao;
+        [ObservableProperty]
+        private string _uf;
+        [ObservableProperty]
+        private string _extintorFisico;
+
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            ExtintorAlmoxarifados = new ObservableCollection<ExtintorAlmoxarifado>();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        //GET - Load list
+        public ICommand GetExtintorAlmoxarifadosCommand =>
+        new Command(async () => await LoadExtintorAlmoxarifadosAsync());
+        private async Task LoadExtintorAlmoxarifadosAsync()
+        {
+            var url = $"{baseUrl}/extintoralmoxarifados";
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode) 
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer.DeserializeAsync<ObservableCollection<ExtintorAlmoxarifado>>(responseStream, _serializerOptions);
+                    ExtintorAlmoxarifados = data;
+                }
+            }
+        }
+
     }
 }
