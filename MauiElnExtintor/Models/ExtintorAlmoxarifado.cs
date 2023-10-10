bbc3f0cc3b69921123673e5d76135b37c613d009
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 21:59:22 -0300

    ExtintorAlmoxarifado

---
+++ MauiElnExtintor/Models/ExtintorAlmoxarifado.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiElnExtintor.Models
+{
+    public class ExtintorAlmoxarifado
+    {
+        public int ExtintorAlmoxarifadoID { get; set; }
+        public string Descricao { get; set; }
+        public string Uf { get; set; }
+        public string ExtintorFisico { get; set; }
+    }
+}
