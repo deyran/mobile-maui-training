9d7d32776f98d63329b9bea9a923bb275adcce02
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 22:49:17 -0300

    MainViewModel

---
+++ MauiElnExtintor/ViewsModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MauiElnExtintor.Models;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Input;
+
+namespace MauiElnExtintor.ViewsModels
+{
+    public class MainViewModel
+    {
+    }
+}
