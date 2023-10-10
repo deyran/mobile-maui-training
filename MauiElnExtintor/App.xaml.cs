a2122eaaf9f390945f7455f6305bd221d8a069ee
Deyvid Rannyere <deyran@gmail.com>
2023-10-09 21:03:42 -0300

    Eletronorte Extintores

---
+++ MauiElnExtintor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiElnExtintor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
