69a3e467371081fec676c2d09d21e6f112f0e594
Deyvid Rannyere <deyran@gmail.com>
2024-01-04 22:39:28 -0300

    FlexLayout

---
+++ Layouts/FlexLayout/FlexLayout.md
@@ -0,0 +1,5 @@
+# FlexLayout
+
+<p align="center"><img src="img01.png" /></p>
+
+FlexLayout is a layout that can organize its children (Horizontally and Vertically) into stack. It can wrap its children if there are to many to fit in a row or column.
--- Layouts/StackLayout/StackLayoutDemo/obj/Debug/net7.0-android/StackLayoutDemo.AssemblyInfo.cs
+++ Layouts/StackLayout/StackLayoutDemo/obj/Debug/net7.0-android/StackLayoutDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StackLayoutDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+09f6db74158fad39fc06b7de83c4719752506b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StackLayoutDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StackLayoutDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
